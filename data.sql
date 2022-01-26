3e2d86803d1c97c2f3d1a0c6dba66f7cc63fe473
eapenzacharias <eapenzac@gmail.com>
2022-01-26 18:16:50 +0100

    Added owners_id to all animals

--- data.sql
+++ data.sql
@@ -64,3 +64,18 @@ UPDATE animals SET species_id = (SELECT id FROM species WHERE name = 'digimon')
 
 UPDATE animals SET species_id = (SELECT id FROM species WHERE name = 'pokemon')
   WHERE species_id IS NULL;
+
+UPDATE animals SET owners_id = (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+  WHERE name = 'Agumon';
+
+UPDATE animals SET owners_id = (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+  WHERE name = 'Gabumon' OR name = 'Pikachu';
+
+UPDATE animals SET owners_id = (SELECT id FROM owners WHERE full_name = 'Bob' )
+  WHERE name = 'Devimon' OR name = 'Plantmon';
+
+UPDATE animals SET owners_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+  WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
+
+UPDATE animals SET owners_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+  WHERE name = 'Angemon' OR name = 'Boarmon';
